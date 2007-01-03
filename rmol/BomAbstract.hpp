105ff0f1500c7ef9bcc8a131211c3d0e09361f73
Denis Arnaud <denis@users.sourceforge.net>
2007-01-03 17:53:16 +0000

    Implemented a new input method, more flexible, given by Nicolai M. Josuttis in his book on the C++ Standrd Library (Addison-Wesley, Aug. 1999, pp655-657).

--- rmol/BomAbstract.hpp
+++ rmol/BomAbstract.hpp
@@ -18,9 +18,12 @@ namespace RMOL {
     virtual ~BomAbstract() {}
 
     /** Dump a Business Object into an output stream.
-        @param ostream& the output stream
-        @return ostream& the output stream. */
+        @param ostream& the output stream. */
     virtual void toStream (std::ostream& ioOut) const {}
+
+    /** Read a Business Object from an input stream.
+        @param istream& the input stream. */
+    virtual void fromStream (std::istream& ioIn) {}
 
   protected:
     /** Protected Default Constructor to ensure this class is abtract. */
@@ -54,6 +57,21 @@ operator<< (std::basic_ostream<charT, traits>& ioOut,
   ioOut << ostr.str();
 
   return ioOut;
+}
+
+/**
+   Piece of code given by Nicolai M. Josuttis, Section 13.12.1 "Implementing
+   Output Operators" (pp655-657) of his book "The C++ Standard Library:
+   A Tutorial and Reference", published by Addison-Wesley.
+ */
+template <class charT, class traits>
+inline
+std::basic_istream<charT, traits>&
+operator>> (std::basic_istream<charT, traits>& ioIn,
+            RMOL::BomAbstract& ioBom) {
+  // Fill Bom object with input stream
+  ioBom.fromStream (ioIn);
+  return ioIn;
 }
 
 #endif // __RMOL_BOMABSTRACT_HPP
