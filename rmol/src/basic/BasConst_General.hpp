97cb459139793a07d595adafabdee44686354c2e
Denis Arnaud <denis@users.sourceforge.net>
2007-01-23 15:50:35 +0000

    Restructuration of the source directories.

---
+++ rmol/src/basic/BasConst_General.hpp
@@ -0,0 +1,15 @@
+#ifndef __RMOL_BAS_BASCONST_GENERAL_HPP
+#define __RMOL_BAS_BASCONST_GENERAL_HPP
+
+// //////////////////////////////////////////////////////////////////////
+// Import section
+// //////////////////////////////////////////////////////////////////////
+
+namespace RMOL {
+
+  /** Default value for the number of draws within the Monte-Carlo
+      Integration algorithm. */
+  extern const int DEFAULT_NUMBER_OF_DRAWS_FOR_MC_SIMULATION;
+  
+}
+#endif
