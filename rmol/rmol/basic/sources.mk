ebb385191ada71dd6055043c91b307c00c0d8c9c
Denis Arnaud <denis@users.sourceforge.net>
2007-02-13 23:19:26 +0000

    Added the missing BasConst_RMOL_Service.hpp file

--- rmol/rmol/basic/sources.mk
+++ rmol/rmol/basic/sources.mk
@@ -1,3 +1,4 @@
 bas_h_sources = $(top_srcdir)/rmol/basic/BasConst_General.hpp \
-		$(top_srcdir)/rmol/basic/BasConst_Overbooking.hpp
+		$(top_srcdir)/rmol/basic/BasConst_Overbooking.hpp \
+		$(top_srcdir)/rmol/basic/BasConst_RMOL_Service.hpp
 bas_cc_sources = $(top_srcdir)/rmol/basic/BasConst.cpp
