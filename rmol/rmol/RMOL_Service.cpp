e79264ab4931feb97353170b1b34acbafc4d69f5
Denis Arnaud <denis@users.sourceforge.net>
2007-02-13 14:03:59 +0000

    Fixed a small bug in the RMOL_Service constructor: the BucketHolder was created, but not stored in the context.

--- rmol/rmol/RMOL_Service.cpp
+++ rmol/rmol/RMOL_Service.cpp
@@ -63,6 +63,7 @@ namespace RMOL {
     _context (iResourceCapacity) {
     BucketHolder& aBucketHolder = 
       FacBucketHolder::instance().create (iResourceCapacity);
+    _context.setBucketHolder (&aBucketHolder);
   }
 
   // //////////////////////////////////////////////////////////////////////
