8c9159453844375781619c5d6625a978a4691b50
Denis Arnaud <denis@users.sourceforge.net>
2009-09-17 16:30:38 +0000

    [Branch 0.23.0] Deleted the deprecated/temporary 0.23.0 release branch corresponding to Ticket #1 (corresponding updates have been integrated within the main branch and within the trunk).

--- 0.23.0/Ticket_1_01/rmol/batches/sources.mk
+++
@@ -1,2 +0,0 @@
-batches_h_sources =
-batches_cc_sources = $(top_srcdir)/rmol/batches/rmol.cpp
--- 0.23.0/Ticket_1_01/rmol/core/sources.mk
+++
@@ -1,4 +0,0 @@
-service_h_sources = $(top_srcdir)/rmol/RMOL_Types.hpp \
-				$(top_srcdir)/rmol/RMOL_Service.hpp \
-				$(top_srcdir)/rmol/RMOL_FORECASTER_Types.hpp
-service_cc_sources =
