d876386e1bc014b2ce78559ef539f305af8bd545
denis <denis@users.sourceforge.net>
2009-12-23 14:45:19 +0000

    [Build] Fixed a missing source file name in the basic layer.

--- stdair/stdair/basic/sources.mk
+++ stdair/stdair/basic/sources.mk
@@ -1,6 +1,6 @@
 bas_h_sources = \
 				$(top_srcdir)/stdair/basic/BasTypes.hpp \
-				$(top_srcdir)/stdair/basic/BasConst_WorldSchedule.hpp \
+				$(top_srcdir)/stdair/basic/BasConst_General.hpp \
 				$(top_srcdir)/stdair/basic/BasConst_Inventory.hpp \
 				$(top_srcdir)/stdair/basic/BasConst_BookingClass.hpp \
 				$(top_srcdir)/stdair/basic/BasConst_Yield.hpp \
