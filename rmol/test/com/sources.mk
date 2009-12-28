15e25ac825defdfae177473de08c99a1ea7cd9d9
Denis Arnaud <denis@users.sourceforge.net>
2009-12-28 15:27:45 +0000

    [Test] Removed the com sub-directory, in order to replace it by an external reference to ExtraCC.

--- rmol/test/com/sources.mk
+++
@@ -1,4 +0,0 @@
-test_com_h_sources = $(top_srcdir)/test/com/CppUnitCore.hpp \
-				$(top_srcdir)/test/com/XmlOutputterHookForCC.hpp
-test_com_cc_sources = $(top_srcdir)/test/com/CppUnitCore.cpp \
-				$(top_srcdir)/test/com/XmlOutputterHookForCC.cpp
