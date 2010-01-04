b56227290cf89c783452e8ae528d4d82591dfb60
denis <denis@users.sourceforge.net>
2010-01-04 20:03:11 +0000

    [Dev] Improved the granularity of the BomManager::display() utility.

--- stdair/stdair/bom/BomManager.cpp
+++ stdair/stdair/bom/BomManager.cpp
@@ -23,66 +23,124 @@ namespace stdair {
 
   // //////////////////////////////////////////////////////////////////////
   void BomManager::display (std::ostream& oStream, const BomRoot& iBomRoot) {
-    
     // Browse the Inventory objects
-    const InventoryList_T lInventoryList= iBomRoot.getInventoryList ();
+    const InventoryList_T& lInventoryList= iBomRoot.getInventoryList ();
     for (InventoryList_T::iterator itInv = lInventoryList.begin();
          itInv != lInventoryList.end(); ++itInv) {
       const Inventory& lCurrentInventory = *itInv;
-      oStream << "Inventory: " << lCurrentInventory.toString();
 
-      // Browse the FlightDate objects
-      const FlightDateList_T lFDList =
-        lCurrentInventory.getFlightDateList ();
-      for (FlightDateList_T::iterator itFD = lFDList.begin();
-           itFD != lFDList.end(); ++itFD) {
-        const FlightDate& lCurrentFD = *itFD;
-        oStream << "Flight-date: " << lCurrentFD.toString();
+      // Call recursively the display() method on the children objects
+      display (oStream, lCurrentInventory);
+    }    
 
-        // Browse the LegDate objects
-        const LegDateList_T lLDList = lCurrentFD.getLegDateList();
-        for (LegDateList_T::iterator itLD = lLDList.begin();
-             itLD != lLDList.end(); ++itLD) {
-          const LegDate& lCurrentLD = *itLD;
-          oStream << "Leg-date: " << lCurrentLD.toString();
-          
-          // Browse the LegCabin objects
-          const LegCabinList_T lLCList = lCurrentLD.getLegCabinList();
-          for (LegCabinList_T::iterator itLC = lLCList.begin();
-               itLC != lLCList.end(); ++itLC) {
-            const LegCabin& lCurrentLC = *itLC;
-            oStream << "Leg-cabin: " << lCurrentLC.toString();
-          }
-        }
-        
-        // Browse the SegmentDate objects
-        const SegmentDateList_T lSDList =
-          lCurrentFD.getSegmentDateList();
-        for (SegmentDateList_T::iterator itSD = lSDList.begin();
-             itSD != lSDList.end(); ++itSD) {
-          const SegmentDate& lCurrentSD = *itSD;
-          oStream << "Segment-date: " << lCurrentSD.toString();
-
-          // Browse the SegmentCabin objects
-          const SegmentCabinList_T lSCList =
-            lCurrentSD.getSegmentCabinList();
-          for (SegmentCabinList_T::iterator itSC = lSCList.begin();
-               itSC != lSCList.end(); ++itSC) {
-            const SegmentCabin& lCurrentSC = *itSC;
-            oStream << "Segment-cabin: " << lCurrentSC.toString();
-
-            // Browse the BookingClass objects
-            const BookingClassList_T lBCList =
-              lCurrentSC.getBookingClassList ();
-            for (BookingClassList_T::iterator itBC = lBCList.begin();
-                   itBC != lBCList.end(); ++itBC) {
-              const BookingClass& lCurrentBC = *itBC;
-              oStream << "Booking class: " << lCurrentBC.toString();
-            }
-          }
-        }
-      }
+  }
+  
+  // //////////////////////////////////////////////////////////////////////
+  void BomManager::display (std::ostream& oStream, const Inventory& iInventory) {
+    // Inventory level
+    oStream << "Inventory: " << iInventory.toString();
+    
+    // Browse the FlightDate objects
+    const FlightDateList_T& lFDList = iInventory.getFlightDateList ();
+    for (FlightDateList_T::iterator itFD = lFDList.begin();
+         itFD != lFDList.end(); ++itFD) {
+      const FlightDate& lCurrentFD = *itFD;
+      
+      // Call recursively the display() method on the children objects
+      display (oStream, lCurrentFD);
     }
-  }    
+  }
+  
+  // //////////////////////////////////////////////////////////////////////
+  void BomManager::display (std::ostream& oStream,
+                            const FlightDate& iFlightDate) {
+    // Flight-date level
+    oStream << "Flight-date: " << iFlightDate.toString();
+    
+    // Browse the LegDate objects
+    const LegDateList_T& lLDList = iFlightDate.getLegDateList();
+    for (LegDateList_T::iterator itLD = lLDList.begin();
+         itLD != lLDList.end(); ++itLD) {
+      const LegDate& lCurrentLD = *itLD;
+      
+      // Call recursively the display() method on the children objects
+      display (oStream, lCurrentLD);
+    }
+    
+    // Browse the SegmentDate objects
+    const SegmentDateList_T& lSDList = iFlightDate.getSegmentDateList();
+    for (SegmentDateList_T::iterator itSD = lSDList.begin();
+         itSD != lSDList.end(); ++itSD) {
+      const SegmentDate& lCurrentSD = *itSD;
+      
+      // Call recursively the display() method on the children objects
+      display (oStream, lCurrentSD);
+    }
+  }
+  
+  // //////////////////////////////////////////////////////////////////////
+  void BomManager::display (std::ostream& oStream,
+                            const LegDate& iLegDate) {
+    // Leg-date level
+    oStream << "Leg-date: " << iLegDate.toString();
+    
+    // Browse the LegCabin objects
+    const LegCabinList_T& lLCList = iLegDate.getLegCabinList();
+    for (LegCabinList_T::iterator itLC = lLCList.begin();
+         itLC != lLCList.end(); ++itLC) {
+      const LegCabin& lCurrentLC = *itLC;
 
+      // Call recursively the display() method on the children objects
+      display (oStream, lCurrentLC);
+    }
+  }
+  
+  // //////////////////////////////////////////////////////////////////////
+  void BomManager::display (std::ostream& oStream,
+                            const LegCabin& iLegCabin) {
+    // Leg-cabin level
+    oStream << "Leg-cabin: " << iLegCabin.toString();
+  }
+  
+  // //////////////////////////////////////////////////////////////////////
+  void BomManager::display (std::ostream& oStream,
+                            const SegmentDate& iSegmentDate) {
+    // Segment-date level
+    oStream << "Segment-date: " << iSegmentDate.toString();
+
+    // Browse the SegmentCabin objects
+    const SegmentCabinList_T& lSCList = iSegmentDate.getSegmentCabinList();
+    for (SegmentCabinList_T::iterator itSC = lSCList.begin();
+         itSC != lSCList.end(); ++itSC) {
+      const SegmentCabin& lCurrentSC = *itSC;
+
+      // Call recursively the display() method on the children objects
+      display (oStream, lCurrentSC);
+    }
+  }
+  
+  // //////////////////////////////////////////////////////////////////////
+  void BomManager::display (std::ostream& oStream,
+                            const SegmentCabin& iSegmentCabin) {
+    // Segment-cabin level
+    oStream << "Segment-cabin: " << iSegmentCabin.toString();
+    
+    // Browse the BookingClass objects
+    const BookingClassList_T& lBCList = iSegmentCabin.getBookingClassList ();
+    for (BookingClassList_T::iterator itBC = lBCList.begin();
+         itBC != lBCList.end(); ++itBC) {
+      const BookingClass& lCurrentBC = *itBC;
+
+      // Call recursively the display() method on the children objects
+      display (oStream, lCurrentBC);
+    } 
+  }
+
+  // //////////////////////////////////////////////////////////////////////
+  void BomManager::display (std::ostream& oStream,
+                            const BookingClass& iBookingClass) {
+    // Booking-class level
+    oStream << "Booking class: " << iBookingClass.toString();
+  }
+  
 }
--- stdair/stdair/bom/BomManager.hpp
+++ stdair/stdair/bom/BomManager.hpp
@@ -16,12 +16,62 @@ namespace stdair {
   /** Utility class for StdAir objects. */
   class BomManager {
   public:
+    // //////////////// Display support methods /////////////////
     /** Recursively display (dump in the underlying output log stream)
         the objects of the given BOM tree.
         @param std::ostream& Output stream in which the BOM tree should be
                logged/dumped.
         @param const BomRoot& Root of the BOM tree to be displayed. */
     static void display (std::ostream&, const BomRoot&);
+
+    /** Recursively display (dump in the underlying output log stream)
+        the objects of the given BOM tree.
+        @param std::ostream& Output stream in which the BOM tree should be
+               logged/dumped.
+        @param const Inventory& Root of the BOM tree to be displayed. */
+    static void display (std::ostream&, const Inventory&);
+
+    /** Recursively display (dump in the underlying output log stream)
+        the objects of the given BOM tree.
+        @param std::ostream& Output stream in which the BOM tree should be
+               logged/dumped.
+        @param const FlightDate& Root of the BOM tree to be displayed. */
+    static void display (std::ostream&, const FlightDate&);
+
+    /** Recursively display (dump in the underlying output log stream)
+        the objects of the given BOM tree.
+        @param std::ostream& Output stream in which the BOM tree should be
+               logged/dumped.
+        @param const LegDate& Root of the BOM tree to be displayed. */
+    static void display (std::ostream&, const LegDate&);
+
+    /** Recursively display (dump in the underlying output log stream)
+        the objects of the given BOM tree.
+        @param std::ostream& Output stream in which the BOM tree should be
+               logged/dumped.
+        @param const LegCabin& Root of the BOM tree to be displayed. */
+    static void display (std::ostream&, const LegCabin&);
+
+    /** Recursively display (dump in the underlying output log stream)
+        the objects of the given BOM tree.
+        @param std::ostream& Output stream in which the BOM tree should be
+               logged/dumped.
+        @param const SegmentDate& Root of the BOM tree to be displayed. */
+    static void display (std::ostream&, const SegmentDate&);
+
+    /** Recursively display (dump in the underlying output log stream)
+        the objects of the given BOM tree.
+        @param std::ostream& Output stream in which the BOM tree should be
+               logged/dumped.
+        @param const SegmentCabin& Root of the BOM tree to be displayed. */
+    static void display (std::ostream&, const SegmentCabin&);
+
+    /** Recursively display (dump in the underlying output log stream)
+        the objects of the given BOM tree.
+        @param std::ostream& Output stream in which the BOM tree should be
+               logged/dumped.
+        @param const BookingClass& Root of the BOM tree to be displayed. */
+    static void display (std::ostream&, const BookingClass&);
   };
 
 }
