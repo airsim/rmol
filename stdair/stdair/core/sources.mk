ee864487e8208c8cc91fc846f8635584d6dfccc8
denis <denis@users.sourceforge.net>
2010-01-28 23:35:01 +0000

    There is now a separate header file for defining (smart) pointer on the STDAIR_Service.

--- stdair/stdair/core/sources.mk
+++ stdair/stdair/core/sources.mk
@@ -1,3 +1,4 @@
 service_h_sources = $(top_srcdir)/stdair/STDAIR_Types.hpp \
-					$(top_srcdir)/stdair/STDAIR_Service.hpp
+					$(top_srcdir)/stdair/STDAIR_Service.hpp \
+					$(top_srcdir)/stdair/STDAIR_ServicePtr.hpp
 service_cc_sources =
