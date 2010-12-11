41cbd92e72d70d3aa6d1fb5056a98a86d40df65d
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2010-12-11 16:54:16 +0100

    [Git] The test samples sub-directory is back to within the test directory.

--- samples/sources.mk
+++
@@ -1,6 +0,0 @@
-sample_csv_sources = \
-	$(top_srcdir)/samples/rm01.csv \
-	$(top_srcdir)/samples/rm02.csv \
-	$(top_srcdir)/samples/rm03.csv \
-	$(top_srcdir)/samples/rm04.csv
-
---
+++ test/samples/sources.mk
@@ -0,0 +1,6 @@
+sample_csv_sources = \
+	$(top_srcdir)/test/samples/rm01.csv \
+	$(top_srcdir)/test/samples/rm02.csv \
+	$(top_srcdir)/test/samples/rm03.csv \
+	$(top_srcdir)/test/samples/rm04.csv
+
