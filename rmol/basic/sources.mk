b46ca423e87ce8ebc1e10390bba8688bc7da1b7d
Anh Quan Nguyen <quan.nguyen@amadeus.com>
2011-02-24 18:01:18 +0100

    Moved YieldRange to stdair. Removed DistributionParameters.

--- rmol/basic/sources.mk
+++ rmol/basic/sources.mk
@@ -1,9 +1,5 @@
 rmol_bas_h_sources = \
 		$(top_srcdir)/rmol/basic/BasConst_General.hpp \
-		$(top_srcdir)/rmol/basic/BasConst_RMOL_Service.hpp \
-		$(top_srcdir)/rmol/basic/DistributionParameters.hpp \
-		$(top_srcdir)/rmol/basic/YieldRange.hpp 
+		$(top_srcdir)/rmol/basic/BasConst_RMOL_Service.hpp
 rmol_bas_cc_sources = \
-		$(top_srcdir)/rmol/basic/BasConst.cpp \
-		$(top_srcdir)/rmol/basic/DistributionParameters.cpp \
-		$(top_srcdir)/rmol/basic/YieldRange.cpp 
+		$(top_srcdir)/rmol/basic/BasConst.cpp
