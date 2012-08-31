50162a2a6b8fe5fb89e180332ab4065875f0dab3
Gabrielle Sabatier <gabrielle.sabatier@amadeus.com>
2012-08-31 15:45:54 +0200

    [Dev] Reset the stdair service shared pointer in order to decrease the count by one every time (and not only when owning the stdair service).

--- rmol/service/RMOL_ServiceContext.cpp
+++ rmol/service/RMOL_ServiceContext.cpp
@@ -52,9 +52,13 @@ namespace RMOL {
 
   // ////////////////////////////////////////////////////////////////////
   void RMOL_ServiceContext::reset() {
-    if (_ownStdairService == true) {
-      _stdairService.reset();
-    }
+
+    // The shared_ptr<>::reset() method drops the refcount by one.
+    // If the count result is dropping to zero, the resource pointed to
+    // by the shared_ptr<> will be freed.
+    
+    // Reset the stdair shared pointer
+    _stdairService.reset();
   }
 
 }
