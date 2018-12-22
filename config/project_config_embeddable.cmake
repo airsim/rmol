4e2b2b37fea779701d9c342874ed08ed149d5f3f
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2018-12-23 00:27:11 +0100

    [Config] Added other versions of Boost.Python

--- config/project_config_embeddable.cmake
+++ config/project_config_embeddable.cmake
@@ -606,7 +606,7 @@ macro (get_boost)
   set (Boost_USE_STATIC_RUNTIME OFF)
   set (BOOST_REQUIRED_COMPONENTS_FOR_LIB
     date_time random iostreams serialization filesystem system
-	locale python27 python34 regex)
+	locale python python27 python34 pytthon37 regex)
   set (BOOST_REQUIRED_COMPONENTS_FOR_BIN program_options)
   set (BOOST_REQUIRED_COMPONENTS_FOR_TST unit_test_framework)
   set (BOOST_REQUIRED_COMPONENTS ${BOOST_REQUIRED_COMPONENTS_FOR_LIB}
