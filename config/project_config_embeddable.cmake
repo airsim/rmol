df3d2e9effe69400c8fa17c80da0e5b142c4678c
Denis Arnaud <denis.arnaud@decathlon.com>
2022-12-31 20:22:58 +0100

    [Dev] Minimum C++ standard is now C++14

--- config/project_config_embeddable.cmake
+++ config/project_config_embeddable.cmake
@@ -75,8 +75,8 @@ endmacro (set_project_versions)
 #  * RUN_GCOV             - Whether or not to perform code coverage
 #
 macro (set_project_options _build_doc _enable_tests _run_gcov)
-  # C++ standard (C++-11)
-  set(CMAKE_CXX_STANDARD 11)
+  # C++ standard (C++-14)
+  set(CMAKE_CXX_STANDARD 14)
   set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
   # Shared libraries
